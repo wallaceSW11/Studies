4f3ebdc7a309630334b140f38b6a7b398b351955
wallacesw11 <wallacecarfreak@hotmail.com>
2022-10-14 21:26:11 -0300

    criando menu no POS

--- Mensageria/POS/Program.cs
+++ Mensageria/POS/Program.cs
@@ -1,16 +1,71 @@
-﻿using Shared;
-using POS;
+﻿using POS;
 
-try
+
+ShowTitle();
+var opcao = Console.ReadLine();
+
+while (opcao != "0")
 {
-    var objectToTransfer = new Shared.ObjectTransfer("1234432112344321", 50);
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Digite o valor da venda:");
+            var valor = Console.ReadLine();
+            Console.WriteLine("Digite o número do cartão:");
+            var numerocartao = Console.ReadLine();
 
-    new Publisher().Publish(objectToTransfer);
+            SendMessage(new Shared.ObjectTransfer(numerocartao, float.Parse(valor)));
+            opcao = null;
+            ShowTitle();
+            break;
+        case "0":
+        default:
+            Console.ReadKey();
+            break;
 
-    // Console.WriteLine($"Object sended!");
+    }
 
 }
-catch (Exception ex)
+
+
+static void SendMessage(Shared.ObjectTransfer obj)
 {
-  Console.WriteLine(ex.Message);
+    try
+    {
+        new Publisher().Publish(obj);
+        Console.WriteLine("Venda realizada.");
+
+    }
+    catch (System.Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        throw;
+    }
 }
+
+
+static void ShowTitle()
+{
+    Console.Clear();
+    Console.WriteLine("**********************");
+    Console.WriteLine("****** WallCard ******");
+    Console.WriteLine("**********************");
+    Console.WriteLine("");
+    Console.WriteLine("Digite a opção desejada:");
+    Console.WriteLine("1 - Nova venda");
+    Console.WriteLine("0 - Sair");
+}
+
+// try
+// {
+//     var objectToTransfer = new Shared.ObjectTransfer("1234432112344321", 50);
+
+//     new Publisher().Publish(objectToTransfer);
+
+//     // Console.WriteLine($"Object sended!");
+
+// }
+// catch (Exception ex)
+// {
+//   Console.WriteLine(ex.Message);
+// }

85f96d25fb98cb0772bf3c24f2f6de9dc5717d96
wallacesw11 <wallacecarfreak@hotmail.com>
2023-03-22 21:13:00 -0300

    creating a new component for money

--- mytasks/src/components/MoneyField.vue
+++ mytasks/src/components/MoneyField.vue
@@ -9,14 +9,11 @@
     @keyup="onKeyUp($event)"
     placeholder="0,00"
     persistent-placeholder
+    max-length="13"
   ></v-text-field>
 </template>
 
 <script>
-
-import { VueMaskDirective } from 'v-mask'
-
-
 export default {
   name: 'MoneyField',
   data() {
@@ -27,95 +24,26 @@ export default {
     }
   },
 
-  directives: {mask: VueMaskDirective},
-
-  watch: {
-    price() {
-      //this.price = newValue.length > 2 ? newValue + ',' : newValue;
-      // this.price = newValue.toLocaleString('pt-BR', {
-      //   style: 'currency',
-      //   currency: "BRL"
-      // })
-    }
-
-    // price(newValue) {
-    //   // console.log(this.price, newValue, this.price.length);
-      
-    //   // if (this.price.length < 6) {
-    //   //   this.priceModel = newValue.substring(2, newValue.length);
-    //   // }
-    // },
-
-    // priceModel(newValue) {
-    //   console.log(newValue);
-    //   this.price = newValue;
-    // }
-  },
-
   methods: {
     onKeyUp() {
       let texto = this.price.split('');
-
-      texto = texto.map(item => item.replace('.', '').replace(',', ''));
-
-      //console.log(texto);
-      //console.log(texto.length);
-
-      if (texto.length == 3) {
-        texto.splice(texto.length-2, 1, ',' + texto[1]);
-
-        console.log(texto);
-
-      } else if (texto.length > 3) {
-        // texto = texto.map(item => item.replace(',', ''));
-        texto.splice(texto.length-2, 1, ',' + texto[texto.length-2]);
-        console.log(texto);
+      texto = texto.filter(item => item != ',' && item != '.')
+      
+      if (texto.length >= 3) {
+         texto.splice(texto.length-2, 1, ',' + texto[texto.length-2]);
       }
 
-      if (texto.length == 7) {
-        texto.splice(texto.length-6, 1, '.' + texto[texto.length-6]);
-
-        console.log(texto);
-
-      } else if (texto.length > 7) {
-        // texto = texto.map(item => item.replace('.', ''));
-        texto.splice(texto.length-6, 1, '.' + texto[texto.length-6]);
-        console.log(texto);
+      if (texto.length >= 6) {
+         texto.splice(texto.length-5, 1, '.' + texto[texto.length-5]);
       }
 
-
-
+      if (texto.length >= 9) {
+         texto.splice(texto.length-8, 1, '.' + texto[texto.length-8]);
+      }
 
       this.price = texto.join('');
-
-
-      // if (this.price.length == 2 ) {
-      //   this.price = this.price + ','
-      // } else if (this.price.length > 2) {
-      //   this.price = this.price.replace(',', '');
-      //   this.price = this.price[]
-      // }
-
-
-      //this.price = this.price.length > 1 ? this.price + ',' : this.price;
-      //this.price = this.price + ','
-
-
-
-      // if (this.price[3] == '0') {
-      //   console.log(this.price.split(' '));
-      //   console.log(this.price.split(' ').splice(3, 1, key.key));
-      //   this.price = this.price.split(' ').splice(3, 1, key.key)
-      // }
-    },
-
-    // onMouseUp(event) {
-    //   let end = event.target.value.length;
-    //   event.target.setSelectionRange(end, end);
-    //   event.target.focus();
-    // }
-  },
-
+    }
+  }
 }
 </script>
 

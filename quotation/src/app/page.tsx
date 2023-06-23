30bd7630744b9332969adb3fa8eba869bf781200
wallacesw11 <wallacecarfreak@hotmail.com>
2023-06-23 20:19:47 -0300

    :construction: adding new

--- quotation/src/app/page.tsx
+++ quotation/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="p-4 min-h-screen bg-gray-800">
       <div className="flex justify-around flex-wrap gap-2">
         <Card quotation="BTC-USD"></Card>
+        <Card quotation="BTC-BRL"></Card>
         <Card quotation="USD-BRL"></Card>
         <Card quotation="CAD-BRL"></Card>
       </div>

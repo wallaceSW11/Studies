7ffbd032ed9ff53600cb8d6f30b4d7d28541f8eb
wallacesw11 <wallacecarfreak@hotmail.com>
2024-01-23 19:56:25 -0300

    :construction: Creating the CRUD

--- allinon/src/models/Task.js
+++ allinon/src/models/Task.js
@@ -1,9 +1,13 @@
 import moment from "moment";
+import { newGuid } from '@/utils/guid';
 
 export default class Task{
   constructor(obj){
     obj = obj || {};
 
+    console.log('a', obj, obj.title);
+
+    this.id = obj.id || newGuid();
     this.createdAt = obj.createdAt || moment().format('YYYY-MM-DD');
     this.title = obj.title;
     this.description = obj.description;
--- allinon/src/views/todo/TodoView.vue
+++ allinon/src/views/todo/TodoView.vue
@@ -1,36 +1,50 @@
 <script setup>
-import { reactive, ref } from "vue";
+import { reactive, ref, toRaw } from "vue";
 import Task from "@/models/Task.js";
 
 let task = reactive(new Task());
+
+let taskInDetail = reactive(new Task());
 let tasks = reactive([]);
 
 let isEditing = ref(false);
 
 // temporary
-tasks.push(new Task({title: 'study vue3', description: 'using vite and vuetify'}));
+//tasks.push(new Task({ title: 'study vue3', description: 'using vite and vuetify' }));
 
 
 const saveTask = () => {
   // if (!task.valid()) return;
 
-  // if (isEditing) {
-  //   /// save
-  //   // return
-  // }
+  if (isEditing.value) {
+    let index = tasks.findIndex(t => t.id == task.id);
+    tasks.splice(index, 1, task);
+    task = reactive(new Task());
+    return;
+  }
 
   tasks.push(new Task(task));
-  task = new Task();
-
-
-
+  task = reactive(new Task());
 }
 
 const editTask = (item) => {
-  task = new Task(item);
-  isEditing = true;
+  let outro = toRaw(item);
+  console.log('outro:', outro.id);
+
+  task = new Task(outro);
+
+  
+  
+  isEditing.value = true;
 }
 
+let openDialog = ref(false);
+
+const showDetail = (item) => {
+  console.log('aqui');
+  taskInDetail = new Task(item);
+  openDialog.value = true;
+}
 
 
 </script>
@@ -40,19 +54,21 @@ const editTask = (item) => {
     <v-layout class="justify-center">
       <v-col cols="12" sm="8" md="6" class="d-flex flex-column bg-surface" style="border: 1px solid black">
         <div class="d-flex justify-center">
-          <h1>TO-DO List</h1>
+          <h1>TO-<i>DO</i> <s>List</s></h1>
         </div>
 
         <div class="d-flex flex-column justify-center px-2">
           <div class="d-flex justify-center">
-            <v-text-field 
-              label="What I need TO DO..." 
+            <v-text-field
+              label="What I need TO DO..."
               color="primary"
               v-model="task.title"
               prepend-inner-icon="mdi-calendar-check-outline"
+              maxlength="50"
+              counter
             ></v-text-field>
           </div>
-  
+
           <div class="d-flex justify-center">
             <v-textarea
               label="Tell me more about that (optional)"
@@ -61,20 +77,22 @@ const editTask = (item) => {
               color="primary"
               v-model="task.description"
               prepend-inner-icon="mdi-details"
+              maxlength="300"
+              counter
             ></v-textarea>
           </div>
 
           <div class="d-flex justify-space-between pb-4">
             <div class="d-flex">
-              <v-checkbox 
-                label="Priority?" 
-                v-model="task.priority" 
-                color="primary" 
-                hide-details=""
+              <v-checkbox
+                label="Priority?"
+                v-model="task.priority"
+                color="primary"
+                hide-details
               >
               </v-checkbox>
             </div>
-  
+
             <div class="d-flex pt-4">
               <v-btn color="primary" @click="saveTask">
                 <v-icon>mdi-plus</v-icon>
@@ -83,43 +101,53 @@ const editTask = (item) => {
             </div>
           </div>
 
-          <hr/>
+          <hr />
 
           <div class="d-flex flex-column pt-2">
-            <div class="d-flex justify-center">
-              <h2>List</h2>
+            <div class="d-flex justify-center py-2">
+              <h2>{{ !!tasks.length ? `You're so busy!!!` : 'Nothing to do!' }}</h2>
             </div>
 
-            <div class="d-flex justify-center" v-if="!tasks.length">
-              <h3>Nothing to do!</h3>
-            </div>
-
-
-            <div v-else class="d-flex flex-column pb-2">
+            <div class="d-flex flex-column pb-2">
               <v-card class="mb-2 bg-secondary-background d-flex" v-for="(item, index) in tasks" :key="index">
                 <v-card-text class="d-flex">
-                  <div class="d-flex">
+                  <div class="d-flex w-100">
                     <span>{{ item.title }}</span>
                   </div>
-                  <div class="d-flex flex-grow-0">
-                    <div class="d-flex flex-grow-0 justify-space-between align-center">
+                  <div class="d-flex">
+                    <div class="d-flex">
                       <v-tooltip text="Edit" location="top">
-                        <template v-slot:activator="{ props }" >
+                        <template #activator="{ props }">
                           <v-icon v-bind="props" @click="editTask(item)">mdi-pencil</v-icon>
                         </template>
                       </v-tooltip>
+                    </div>
+
+                    <div class="d-flex px-2">
                       <v-tooltip text="Delete" location="top">
-                        <template v-slot:activator="{ props }" >
-                          <v-icon v-bind="props" @click="deleteTask(item)" class="py-2">mdi-delete</v-icon>
-                        </template>
-                      </v-tooltip>
-                      <v-tooltip :text="item.done ? 'undone' : 'done'" location="top">
-                        <template v-slot:activator="{ props }" >
-                          <v-icon v-bind="props" @click="toggleDone(item)">{{ item.done ? 'mdi-timer-alert-outline' : 'mdi-check' }}</v-icon>
+                        <template #activator="{ props }">
+                          <v-icon v-bind="props" @click="deleteTask(item)" class="px-2">mdi-delete</v-icon>
                         </template>
                       </v-tooltip>
                     </div>
-  
+
+                    <div class="d-flex">
+                      <v-tooltip :text="item.done ? 'Undone' : 'Done'" location="top">
+                        <template #activator="{ props }">
+                          <v-icon v-bind="props" @click="toggleDone(item)">{{ item.done ? 'mdi-timer-alert-outline' :
+                            'mdi-check' }}</v-icon>
+                        </template>
+                      </v-tooltip>
+
+                    </div>
+
+                    <div class="d-flex pl-2">
+                      <v-tooltip text="Detail" location="top">
+                        <template #activator="{ props }">
+                          <v-icon v-bind="props" @click="showDetail(item)">mdi-eye</v-icon>
+                        </template>
+                      </v-tooltip>
+                    </div>
                   </div>
                 </v-card-text>
               </v-card>
@@ -131,22 +159,33 @@ const editTask = (item) => {
         </div>
       </v-col>
     </v-layout>
-    
+
   </v-container>
 
-  <v-dialog
-    v-model="showDialog"
-  >
-    <v-card>
+  <v-dialog v-model="openDialog" width="50%">
+    <v-card class="pa-3">
       <v-card-title>
-        <span>{{ task.title }}</span>
+        <span>{{ taskInDetail.title }}</span>
       </v-card-title>
 
       <v-card-text>
-        <v-textarea >
-
-        </v-textarea>
+        <v-row>
+          <v-col cols="10">
+            <span>Created at: {{ taskInDetail.createdAt }}</span>
+          </v-col>
+          <v-col cols="2">
+            <span>{{ taskInDetail.priority }}</span>
+          </v-col>
+          <v-col cols="12">
+            <span>{{ taskInDetail.description }}</span>
+          </v-col>
+        </v-row>
       </v-card-text>
+      <v-card-actions class="d-flex justify-end">
+        <v-btn @click="openDialog = false" variant="outlined" color="secondary">
+          Close
+        </v-btn>
+      </v-card-actions>
     </v-card>
 
   </v-dialog>

d90924ecb6567bd1dcf603a321e5a62c85bdc62c
wallacesw11 <wallacecarfreak@hotmail.com>
2024-03-01 13:18:03 -0300

    Retirando projeto miles

--- Miles/src/utils/guid.js
+++
@@ -1,9 +0,0 @@
-import { v4 as uuidv4 } from 'uuid';
-
-function newGuid() {
-  return uuidv4();
-}
-
-export {
-  newGuid
-}
--- Miles/src/utils/language.js
+++
@@ -1,9 +0,0 @@
-
-import STORAGE_DATA from "@/constants/storage-constants";
-import storageAPI from "@/service/api/storageAPI";
-
-function language() {
-  return storageAPI.get(STORAGE_DATA.CONFIGURATIONS.LANG.key) || 'en';
-}
-
-export default language;
